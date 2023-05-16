411798e46d9b8a1c8be0f2871169456afc2679af
Stivendps <121703665+Stivendps@users.noreply.github.com>
2023-05-15 20:20:41 -0500

    Add files via upload

---
+++ Planos.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Planos
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int numeroTrabajadores;
+        trabajadores[] lostrabajadores = new trabajadores[50];
+        String identificacion, nombre, area;
+        double semana1, semana2, semana3,semana4;
+        double promedio = 0;
+        System.out.println("Digite la cantidad de trabajadores: ");
+        numeroTrabajadores = sc.nextInt();
+        for(int i = 0; i < numeroTrabajadores; i++){
+            System.out.println("Digite la identificacion del trabajador: ");
+            identificacion = sc.next();
+            System.out.println("Digite el nombre del trabajador: ");
+            nombre = sc.next();
+            System.out.println("Digite el area del trabajador: ");
+            area = sc.next();
+            System.out.println("Digite los planos terminados en la semana 1 del Trabajador: ");
+            semana1 = sc.nextDouble();
+            System.out.println("Digite los planos terminados en la semana 2 del Trabajador: ");
+            semana2 = sc.nextDouble();
+            System.out.println("Digite los planos terminados en la semana 3 del Trabajador: ");
+            semana3 = sc.nextDouble();
+            System.out.println("Digite los planos terminados en la semana 4 del Trabajador: ");
+            semana4 = sc.nextDouble();
+            trabajadores unTrabajador = new trabajadores();
+            unTrabajador.identificacion = identificacion;
+            unTrabajador.nombre = nombre;
+            unTrabajador.area = area;
+            unTrabajador.semanauno = semana1;
+            unTrabajador.semanados = semana2;
+            unTrabajador.semanatres = semana3;
+            unTrabajador.semanacuatro = semana4;
+            lostrabajadores[i] = unTrabajador;
+        }
+        
+        for(int i =0;i < numeroTrabajadores;i++){
+            double sum = (lostrabajadores[i].semanauno + lostrabajadores[i].semanados + lostrabajadores[i].semanatres +lostrabajadores[i].semanacuatro)/4;
+            promedio = promedio + sum / numeroTrabajadores;
+        }
+        System.out.println("el promedio de planos del area a la  semana es: "+ promedio);
+    }    
+}
---
+++ trabajadores.java
@@ -0,0 +1,13 @@
+public class trabajadores
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+   String identificacion;
+   String nombre;
+   String area;
+   double semanauno;
+   double semanados;
+   double semanatres;
+   double semanacuatro;
+}
